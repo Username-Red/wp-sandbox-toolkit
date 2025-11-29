0da4233fa5d15c5fda53c60093f3359efc84ff68
Username-Red <ashkestrel@gmail.com>
2025-11-29 21:18:00 +0200

    updating all components to check action works

--- assets/js/plugin-scripts.js
+++ assets/js/plugin-scripts.js
@@ -1,5 +1,2 @@
 // Frontend plugin JS
-console.log("My Custom Boilerplate Plugin JS loaded!");
-console.log("Updated to version 3.0.7");
-
-alert("Hey it worked yay");
+console.log("Plugin version 4.0.1");
